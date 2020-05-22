2e3d5c3bc2f74890ea6876e767fa8fcd3bb50848
Krishna kanth Mundada <kmun0005@student.monash.edu>
2020-05-22 16:53:22 +1000

    templates added

--- mission_to_mars_flask/app.py
+++ mission_to_mars_flask/app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     return render_template('login.html')
+#
+#
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     return render_template('signup.html')
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('login.html')
+
+
+@app.route('/notfound', methods=['GET', 'POST'])
+def error():
+    return render_template('404.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
