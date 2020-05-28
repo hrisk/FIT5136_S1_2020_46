0b3a1012f47959bfe55dccf39a2e58e15a1c2701
Krishna kanth Mundada <kmun0005@student.monash.edu>
2020-05-28 17:35:45 +1000

    login and signup is working
    login and signup is working but needs some work around the forgot passwork and required fields in signup. can use is authenticated for other pages. Also, need to work on having seperate candidate login and admin login on the same page

---
+++ mission_to_mars_flask/model_employee.py
@@ -0,0 +1,11 @@
+from flask_login import UserMixin
+from . import db
+
+
+class Employee(UserMixin, db.Model):
+    employee_id = db.Column(db.Integer, primary_key=True)
+    employee_name = db.Column(db.String(100))
+    employee_title = db.Column(db.String(100))
+    employee_permissions = db.Column(db.String(10000))
+    employee_email = db.Column(db.String(100), unique=True)
+    employee_password = db.Column(db.String(100))
