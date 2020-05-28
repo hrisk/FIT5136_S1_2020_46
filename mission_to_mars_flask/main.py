7ed70e6f07e5059f3bfc34cd9b1a4027410c9c68
Krishna kanth Mundada <kmun0005@student.monash.edu>
2020-05-28 23:54:33 +1000

    shuttle view added

--- mission_to_mars_flask/main.py
+++ mission_to_mars_flask/main.py
@@ -22,6 +22,13 @@ def profile():
     return render_template(template_name_or_list='profile.html', name=current_user.employee_name)
 
 
+@main.route(rule='/shuttle')
+def shuttle():
+    mission_list = ['Mission A', 'Mission B']
+    shuttle_list = ['Shuttle A', 'Shuttle B']
+    return render_template(template_name_or_list='feature2.html', mission_list=mission_list, shuttle_list=shuttle_list)
+
+
 # TODO: proper error handler
 @main.errorhandler(404)
 def not_found(e):
