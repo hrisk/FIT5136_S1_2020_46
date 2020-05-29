ae6fcf4ef2003172cc5193a42795c36d8ced4d2a
RUNZE ZHANG <rzha0081@student.monash.edu>
2020-05-29 10:17:20 +1000

    add details for candidate model

--- mission_to_mars_flask/model_candidate.py
+++ mission_to_mars_flask/model_candidate.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from . import db
+import datetime
 
 
 # TODO: finish the candidate model and add the relevant columns in database
@@ -8,6 +9,19 @@ class Candidate(UserMixin, db.Model):
     candidate_name = db.Column(db.String(100))
     candidate_email = db.Column(db.String(100), unique=True)
     candidate_password = db.Column(db.String(100))
+
+    candidate_dob = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    candidate_phone = db.Column(db.String(20), unique=True, nullable=False)
+    candidate_identification_type = db.Column(db.String(10), nullable=False)
+    candidate_gender = db.Column(db.String(10), nullable=False)
+    candidate_allergies = db.Column(db.String(20, nullable=False))
+    candidate_food_preference = db.Column(db.String(20), nullable=False)
+    candidate_qualification = db.Column(db.String(20))
+    candidate_experience = db.Column(db.String(20))
+    candidate_occupation = db.Column(db.String(50))
+    candidate_computer_skills = db.Column(db.String(30), nullable=False)
+    candidate_languages_known = db.Column(db.String(100))
+
 
     def get_id(self):
         return self.candidate_id
